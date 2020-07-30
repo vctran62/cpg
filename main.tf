62fb3fd9ded0959b3a99e6bb5d824c8e345436af
Vinh Tran <vctran62@users.noreply.github.com>
2020-07-30 13:33:53 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  subscription_id = var.subscriptionId
+  client_id       = var.clientId
+  client_secret   = var.clientSecret
+  tenant_id       = var.tenantId
+  version = "=1.44.0"
+  #features {}
+}
---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "subscriptionId" {}
+variable "clientId" {}
+variable "clientSecret" {}
+variable "tenantId" {}
+variable "region" {}
+
+
+variable "agent_count" {
+    default = 3
+}
+
+variable "ssh_public_key" {
+    default = "~/.ssh/id_rsa.pub"
+}
+
+variable "dns_prefix" {
+    default = "k8stest"
+}
+
+variable "cluster_name" {
+    default = "k8stest"
+}
+
+variable "resource_group_name" {
+    default = "azure-k8stest"
+}
+
+variable "location" {
+    default = "West US"
+}
+
+variable "log_analytics_workspace_name" {
+    default = "testLogAnalyticsWorkspaceName"
+}
+
+# refer https://azure.microsoft.com/global-infrastructure/services/?products=monitor for log analytics available regions
+variable "log_analytics_workspace_location" {
+    default = "westus"
+}
+
+# refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing 
+variable "log_analytics_workspace_sku" {
+    default = "PerGB2018"
+}
+
+variable "environment" {
+    default = "Development"
+}
